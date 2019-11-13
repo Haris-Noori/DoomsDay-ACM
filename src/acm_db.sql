6fee236b0838fb4c507fb170e802355268276012
Haris <harisnoori20@gmail.com>
2019-11-14 00:41:14 +0500

    database file

--- src/acm_db.sql
+++ src/acm_db.sql
@@ -1,14 +1,69 @@
-/* DATABASE FILE
-DATABASE NAME: acm_db
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2019 at 08:40 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
-DROP DATABASE IF EXISTS acm_db;
-CREATE DATABASE acm_db;
-USE acm_db;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE participants(
-    p_id INT(5),
-    p_name VARCHAR(20),
-    p_cnic INT(13),
-    p_phone INT(11)
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `acm_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_name` varchar(30) NOT NULL,
+  `admin_pass` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`admin_name`, `admin_pass`) VALUES
+('root', 'root');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+CREATE TABLE `participants` (
+  `p_id` int(5) DEFAULT NULL,
+  `p_name` varchar(20) DEFAULT NULL,
+  `p_cnic` int(13) DEFAULT NULL,
+  `p_phone` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD UNIQUE KEY `admin_name` (`admin_name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
