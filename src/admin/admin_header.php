565007e618c0d03a82db169b5d7d5fa66e579bbe
Haris <harisnoori20@gmail.com>
2019-11-23 04:10:49 +0500

    Revenue generating

--- src/admin/admin_header.php
+++ src/admin/admin_header.php
@@ -40,7 +40,7 @@
                 <a href="remove_operator.php" class="list-group-item list-group-item-action bg-light">Remove Operator</a>
                 <a href="registered_participants.php" class="list-group-item list-group-item-action bg-light">Registered Participants</a>
                 <a href="delete_participant.php" class="list-group-item list-group-item-action bg-light">Delete Registration</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Revenue</a>
+                <a href="revenue.php" class="list-group-item list-group-item-action bg-light">Revenue</a>
                 <a href="#" class="list-group-item list-group-item-action bg-light">All Team</a>
             </div>
         </div>
